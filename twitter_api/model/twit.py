e810fe53bb6e4f5a5db4a139548a5d1e7622b975
Eplayrus <31314099+Eplayrus@users.noreply.github.com>
2024-12-31 00:34:22 +0300

    Delete twitter_api directory

--- twitter_api/model/twit.py
+++
@@ -1,7 +0,0 @@
-class Twit:
-    def __init__(self, body, author):
-        self.body = body
-        self.author = author
-
-    def to_dict(self):
-        return {"body": self.body, "author": self.author}
